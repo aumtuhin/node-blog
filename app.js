aa624e24d09b205c9c02375ef180f318363a58c8
aumtuhin <dev.aumtuhin@gmail.com>
2021-03-09 17:13:07 +0100

    added router file and home route

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const  express = require('express');
-
-
 // intit app
-const app = express();
+const app = express();  
+// routes
+const router = require('./router');
 
-app.get('/', (req, res) => {
-    res.send({
-        msg: 'hello'
-    });
-});
+//set up views folder
+app.set('views', 'views');
+ //view engine
+ app.set('view engine', 'ejs');
+ //statci files
+ app.use(express.static('public'));
+
+app.use('/', router);
 
 app.listen(8080, () => {
     console.log('app is listening on port 8080');
