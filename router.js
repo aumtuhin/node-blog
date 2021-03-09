aa6854ff4e418769960d0ecb6b2e5f3b616c53fd
aumtuhin <dev.aumtuhin@gmail.com>
2021-03-09 17:17:47 +0100

    added about route

--- router.js
+++ router.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router =  express.Router();
 
+//home route
 router.get('/', (req, res) => {
     res.render('home-guest');
+});
+
+//about route
+router.get('/about', (req, res) => {
+    res.send('This is our about');
 });
 
 
