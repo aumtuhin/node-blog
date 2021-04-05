eed9f85b0507f31815a8fdf424320ec4523c17ce
aumtuhin <dev.aumtuhin@gmail.com>
2021-04-05 13:42:40 +0200

    issue fixed to save post into database

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,7 +15,7 @@ exports.mustBeLoogedIn = function(req, res, next) {
 exports.login = (req, res) => {
     let user = new User(req.body);
     user.login().then((result) => {
-        req.session.user = { avatar: user.avatar, username: user.data.username }
+        req.session.user = { avatar: user.avatar, username: user.data.username, _id: user.data._id }
         req.session.save(() => {
             res.redirect('/');
         });
@@ -36,7 +36,7 @@ exports.logout = (req, res) => {
 exports.register = (req, res) => {
     let user = new User(req.body);
     user.register().then(() => {
-        req.session.user = { avatar: user.avatar, username: user.data.username }
+        req.session.user = { avatar: user.avatar, username: user.data.username, _id: user.data._id }
         req.session.save(() => {
             res.redirect('/');
         });
